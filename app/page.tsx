decf8f6e994ab22badf2121d4e595384eb7db73c
apaluchdev <apaluchdev@gmail.com>
2024-07-10 22:08:34 -0400

    Disable caching of the tasks

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import TaskList from "@/components/task/task-list";
 import { db } from "@/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
