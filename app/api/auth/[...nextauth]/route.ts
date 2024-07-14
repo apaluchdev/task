be16fadd6126e1a66526b185be49a40ff95835d7
apaluchdev <apaluchdev@gmail.com>
2024-07-13 20:45:02 -0400

    Testing a credentials provider implementation

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import GithubProvider from "next-auth/providers/github";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/db";
 import { Adapter } from "next-auth/adapters";
+import Credentials from "next-auth/providers/credentials";
 
 const handler = NextAuth({
   adapter: DrizzleAdapter(db) as Adapter,
@@ -11,23 +12,35 @@ const handler = NextAuth({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
+    Credentials({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
 
-    // Credentials({
-    //   credentials: {
-    //     username: { label: "Username" },
-    //     password: { label: "Password", type: "password" },
-    //   },
-    //   async authorize(credentials) {
-    //     const response = await fetch(request)
-    //     if (!response.ok) return null
-    //     return (await response.json()) ?? null
-    //   },
-    // }),
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
   ],
   callbacks: {
     session({ session, token, user }) {
-      // `session.user.id` is now a valid property, and will be type-checked
-      // in places like `useSession().data.user` or `auth().user`
       return {
         ...session,
         user: {
