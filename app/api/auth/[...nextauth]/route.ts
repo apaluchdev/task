2b6b6bc2976f080fda9b84dbc781124e16152a39
apaluchdev <apaluchdev@gmail.com>
2024-07-14 09:41:50 -0400

    Switched provider order

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -12,13 +12,13 @@ const handler = NextAuth({
   },
   adapter: DrizzleAdapter(db) as Adapter,
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
   callbacks: {
